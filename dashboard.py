4a6d9fce10e95ff8b6db659d1b208b86e51b85eb
András Veres-Szentkirályi <vsza@vsza.hu>
2011-03-13 21:25:08 +0100

    core: sort TODOs by deadline

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 from django.template import Template, Context
 from django.conf import settings
 from config import Config
+import datetime
 import sys
 import os
 
@@ -19,13 +20,26 @@ def loadcfg():
 def getTodos(acc, module):
 	mlist = module.getTodos()
 	cn = module.__class__.__name__
+	noon = datetime.time(12)
+	now = datetime.datetime.now()
+	dl = datetime.datetime.now() + datetime.timedelta(weeks = 10)
 	for todo in mlist:
 		todo['src'] = cn
+		if 'deadline' not in todo:
+			todo['deadline_cmp'] = dl
+		elif type(todo['deadline']) == datetime.date:
+			todo['deadline_cmp'] = datetime.datetime.combine(todo['deadline'], noon)
+		elif type(todo['deadline']) != datetime.datetime:
+			todo['deadline_cmp'] = dl
+		else:
+			todo['deadline_cmp'] = todo['deadline']
+		todo['late'] = todo['deadline_cmp'] < now
 	return acc + mlist
 
 cvals = loadcfg()
 mods = [getattr(__import__(m), m.capitalize())() for m in cvals['modules'].split(',')]
 todos = reduce(getTodos, mods, [])
+todos.sort(lambda x, y: cmp(x['deadline_cmp'], y['deadline_cmp']))
 
 tpl = None
 settings.configure()

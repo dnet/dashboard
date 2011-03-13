4dcfa85c7083eba4b56092c9d9dd548ae08d0a4b
András Veres-Szentkirályi <vsza@vsza.hu>
2011-03-13 11:29:28 +0100

    core: check for empty module config

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,11 @@
 from config import Config
+import sys
 
 modnames = str(Config().value('core/modules').toString())
+if modnames == '':
+	print 'No modules are configured, check your configuration!'
+	sys.exit(1)
+
 mods = [getattr(__import__(m), m.capitalize())() for m in modnames.split(',')]
 todos = reduce(lambda a, e: a + e.getTodos(), mods, [])
 

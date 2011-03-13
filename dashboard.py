7a25aea19a439c01c7f0814b7d5e8e2bd7ef44cb
András Veres-Szentkirályi <vsza@vsza.hu>
2011-03-13 21:24:36 +0100

    core: switch to HTML output

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,37 @@
+from __future__ import with_statement
+from django.template import Template, Context
+from django.conf import settings
 from config import Config
 import sys
+import os
 
-modnames = str(Config().value('core/modules').toString())
-if modnames == '':
-	print 'No modules are configured, check your configuration!'
-	sys.exit(1)
+def loadcfg():
+	cfg = Config()
+	ckeys = ['modules', 'output', 'template']
+	cvals = dict()
+	for key in ckeys:
+		cvals[key] = str(cfg.value('core/' + key).toString())
+		if cvals[key] == '':
+			print 'Key %s is not configured, check your configuration!' % key
+			sys.exit(1)
+	return cvals
 
-mods = [getattr(__import__(m), m.capitalize())() for m in modnames.split(',')]
-todos = reduce(lambda a, e: a + e.getTodos(), mods, [])
+def getTodos(acc, module):
+	mlist = module.getTodos()
+	cn = module.__class__.__name__
+	for todo in mlist:
+		todo['src'] = cn
+	return acc + mlist
 
-for todo in todos:
-	print todo['title']
-	print "\t", todo['subtitle']
+cvals = loadcfg()
+mods = [getattr(__import__(m), m.capitalize())() for m in cvals['modules'].split(',')]
+todos = reduce(getTodos, mods, [])
+
+tpl = None
+settings.configure()
+with open(cvals['template'], 'r') as f:
+	tpl = Template(f.read())
+
+html = tpl.render(Context({'todos': todos}))
+with open(cvals['output'], 'w') as f:
+	f.write(html.encode('utf-8'))
